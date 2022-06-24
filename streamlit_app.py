61d4f6d6c0572882fae345fa57930ca8293ad238
Andrew Lane <AndrewLane@users.noreply.github.com>
2022-06-24 15:08:38 -0400

    ask for fruity advice

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # put the data into a df
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
