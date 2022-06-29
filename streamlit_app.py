a3f8e93cd563b00dfcb03cb29c7fd61272ae4245
Andrew Lane <AndrewLane@users.noreply.github.com>
2022-06-29 15:43:28 -0400

    prompt to add a fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,7 @@ my_cur.execute("SELECT * from fruityvice")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+
+fruit_to_add_choice = streamlit.text_input('What fruit would you like to add?','DragonFruit')
+streamlit.write('Thanks for adding ', fruit_to_add_choice)
